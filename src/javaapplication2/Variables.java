84a8dd2032168865b2292de8f194017278a6a297
juanAriel <arielvia2013@gmail.com>
2021-10-19 15:20:24 -0400

    declaracion de variables en una clase  las varaibles siempre tienen un tipo definido y no pueden contener valores de diferentes tipos

---
+++ src/javaapplication2/Variables.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author ariel
+ */
+public class Variables {
+    public static void main(String[] args) {
+        int variable1;
+        int variable2, variable3;
+        int variable4=23;
+        
+        variable1=6;
+        
+        System.err.println("variable1 contiene el valor: "+ variable1);
+        
+        variable2 = 2* variable1;
+        
+        System.err.println("variable2 contiene el valor: "+ variable2);
+        
+    }
+}
