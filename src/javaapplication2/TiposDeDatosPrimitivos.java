d68822bded36952cd524ae1fa5dd340ed8393996
juanAriel <arielvia2013@gmail.com>
2021-10-19 15:42:27 -0400

    datos primitivos que podemos usar siempre y cuando necesitemos variables faciles y no tan complejas en nuestars funciones

---
+++ src/javaapplication2/TiposDeDatosPrimitivos.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author ariel
+ */
+public class TiposDeDatosPrimitivos {
+    public static void main(String[] args) {
+        byte elMenorByte = Byte.MIN_VALUE;
+        byte elMayorByte = Byte.MAX_VALUE;
+        System.out.println("el mayor y menor byte son: "+ elMenorByte +" y "+elMayorByte);
+        
+        System.out.println("el mayor y menor Short son: "+ Short.MAX_VALUE +" y "+ Short.MIN_VALUE);
+                
+        System.out.println("el mayor y menor int son: "+ Integer.MAX_VALUE +" y "+Integer.MIN_VALUE);
+        
+        System.out.println("el mayor y menor long son: "+ Long.MAX_VALUE +" y "+Long.MIN_VALUE);
+        
+        float variableFloat = 5.6f;
+        
+        double variableDouble = 5.6;
+        
+        char a = 'a';
+        
+        boolean trueBoolean = true;
+        boolean falseBoolean= false;
+        
+        
+        
+    }
+}
